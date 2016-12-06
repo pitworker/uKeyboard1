6be469c00dbfb2b838e5309906f6d22d8d1581ff
IZ <iz@Isaacs-MacBook-Air.local>
2016-12-06 16:18:37 -0500

    Added working segue to project, and keyboard.xib for the actual keyboard
    Sparrow: make the actual keyboard in the keyboard.xib view file, which
    can be located in the keyboard folder once you are in Xcode. And guys,
    please try to find a better way to do a collection segue, take a look
    at the main storyboard and you will know why.

---
+++ uKeyboard1/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  uKeyboard1
+//
+//  Created by Isaac Zhu on 12/6/16.
+//  Copyright © 2016 uKeyboard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ uKeyboard1/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  uKeyboard1
+//
+//  Created by Isaac Zhu on 12/6/16.
+//  Copyright © 2016 uKeyboard. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var firstRow: UIStackView!
+    @IBOutlet weak var secondRow: UIStackView!
+    @IBOutlet weak var thirdRow: UIStackView!
+    @IBOutlet weak var fourthRow: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
