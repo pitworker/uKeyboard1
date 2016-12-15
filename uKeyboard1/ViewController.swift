6606291d16e32b4e383c33d0461f2137b67a5c2b
IZ <iz@Isaacs-MacBook-Air.local>
2016-12-15 09:49:36 -0500

    Here we go, added a slide in/out menu for category selection.
    Now we have a slide menu to select different categories of keys.
    Clicking inside the menu should alter the main view.

--- uKeyboard1/ViewController.swift
+++ uKeyboard1/ViewController.swift
@@ -7,15 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var types = [String]()
-
-    @IBOutlet weak var senderName: UILabel!
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var trailing: NSLayoutConstraint!
+    var menuShowing = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        types = ["Latin Characters", "Arabic Numbers", "Punctuations", "Emojis", "Other Characters"]
-        self.senderName.text = senderKey
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +21,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return types.count
+    @IBAction func expandMenu(_ sender: Any) {
+        if (menuShowing){
+            trailing.constant = 0
+        }
+        else{
+            trailing.constant = -200
+        }
+        menuShowing = !menuShowing 
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CategoryTableViewCell
-        cell.textLabel?.text = types[indexPath.row]
-        return cell
-    }
-
 }
 
